05ae7491a5d35afbb54f72363d326a8de7026d9f
sreckokuz <sreckokuz@yahoo.com>
2020-03-28 00:55:27 +0100

    All logic of promise functionality

---
+++ somefunc.js
@@ -0,0 +1,26 @@
+    function somefunction() {
+        var promise = new Promise((resolve, reject) => {
+            let error = true
+            setTimeout(() => {
+                console.log("srecko");
+                if (error) {
+                    reject("Error")
+                } else {
+                    resolve("This is OK");
+                }
+            }, 2000)
+        });
+        return promise;
+    }
+
+
+    somefunction().then(
+        (success) => {
+            console.log(success)
+        }
+    ).catch((error) => {
+        console.log(error)
+    });
+
+
+
